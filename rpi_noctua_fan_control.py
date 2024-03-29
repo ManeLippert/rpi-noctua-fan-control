1ac45a8b04ac16b3118b4d21d05228a4792c9b5b
Manuel Lippert <manny.lippert@gmail.com>
2024-03-29 13:19:27 +0100

    Update rpi_noctua_fan_control.py

--- rpi_noctua_fan_control.py
+++ rpi_noctua_fan_control.py
@@ -26,7 +26,7 @@ PWM_FREQ = 25           # [Hz] PWM frequency
 FAN_PIN = 15            # BCM pin used to drive PWM fan
 
 OFF_TEMP = 40           # [°C] temperature below which to stop the fan
-MIN_TEMP = 50           # [°C] temperature above which to start the fan
+MIN_TEMP = 40           # [°C] temperature above which to start the fan
 MAX_TEMP = 70           # [°C] temperature at which to operate at max fan speed
 
 FAN_LOW = 1
